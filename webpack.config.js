3600dec78181a56ccc54bb67b090cd809c76383e
Sagar <sagarrajak858@gmail.com>
2022-11-07 05:03:49 +0530

    added bundle optimization

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,54 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CompressionPlugin = require("compression-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 const stylesHandler = MiniCssExtractPlugin.loader;
 
 module.exports = {
-    entry: './src/index.tsx',
-    output: {
-        path: path.join(__dirname, "dist"),
-        filename: "[contenthash].[name].js"
-    },
-    resolve: {
-        extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, "src", "index.html")
-        }),
-        new MiniCssExtractPlugin(),
+  entry: "./src/index.tsx",
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[contenthash].[name].js",
+    clean: true,
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "index.html"),
+    }),
+    new MiniCssExtractPlugin(),
+    new CompressionPlugin({
+        algorithm: "gzip",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/i,
+        use: ["ts-loader"],
+        exclude: ["/node_modules/"],
+      },
+      {
+        test: /\.(s[ac]ss)$/i,
+        use: [stylesHandler, "css-loader", "postcss-loader", "sass-loader"],
+      },
+      {
+        test: /\.(css)$/i,
+        use: [stylesHandler, "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset",
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.(ts|tsx)$/i,
-                use: ["ts-loader"],
-                exclude: ["/node_modules/"],
-            },
-            {
-                test: /\.(s[ac]ss)$/i,
-                use: [stylesHandler, "css-loader", "postcss-loader", "sass-loader"]
-            },
-            {
-                test: /\.(css)$/i,
-                use: [stylesHandler, "css-loader", "postcss-loader"]
-            },
-            {
-                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-                type: "asset",
-            },
-        ]
-    }
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+  },
 };
